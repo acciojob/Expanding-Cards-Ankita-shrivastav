505daff0fe8f88f0d9384eb9441b4412456cc34c
Ankita-shrivastav <93003257+Ankita-shrivastav@users.noreply.github.com>
2023-12-22 02:41:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+// const panels=document.querySelectorAll('.panel');
+// panels.forEach(panel=>{
+// 	panel.addEventListener('click',()=>{
+// 		removeActiveClasses();
+// 		panel.classList
+// 	})
+// })
+const panels = document.querySelectorAll('.panel');
+
+function toggleOpen() {
+  this.classList.toggle('active');
+}
+
+function toggleActive(e) {
+  if (e.propertyName.includes('flex')) {
+    this.classList.toggle('open-active');
+  }
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
